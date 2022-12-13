7b2d3d61ba04646a31c0bb3323ddc6520ca5e82a
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-12-13 08:06:03 -0500

    answers questions

--- src/gimkitBot/BasicFunctions.java
+++ src/gimkitBot/BasicFunctions.java
@@ -11,8 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 public class BasicFunctions {
-    private String id;
-    private String name;
+    private String id, name, prevQuestion;
     private ChromeDriver driver;
     private static Map<String, String> words;
 
@@ -24,61 +23,57 @@ public class BasicFunctions {
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
         //options.addArguments("--allowed-ips");
-
         driver = new ChromeDriver(options);
     }
 
-    public ChromeDriver getDriver() {
-        return driver;
-    }
+    public ChromeDriver getDriver() {return driver;}
 
     public void log_in() throws InterruptedException {
         driver.get("https://gimkit.com/join");
-
         Thread.sleep(500);
 
         WebElement idBox = driver.findElement(By.xpath("//input[@class='sc-iIHjhz gODgKf']"));
         idBox.sendKeys(id);
         idBox.sendKeys(Keys.ENTER);
-
         Thread.sleep(1000);
 
         WebElement nBox = driver.findElement(By.xpath("//input[@class='sc-iIHjhz gODgKf']"));
         nBox.sendKeys(name);
         nBox.sendKeys(Keys.ENTER);
         
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
     }
 
     public String findAns(String question) {
-        String possibleAns;
-        for (String key : words.keySet()) {
-            possibleAns = words.get(key);
-            if (possibleAns.equals(question)) {return possibleAns;}
-        }
+        for (String key : words.keySet()) {if (key.equals(question)) {return words.get(key);}}
         return null;
     }
 
     public void multipleChoice() {
-        WebElement pAns;
+        WebElement pAns; String cAns; int i;
         List<WebElement> preQAA = driver.findElements(By.xpath("//span[@class='notranslate lang-en']"));
 
-        String question = preQAA.get(0).getText();
-
-        String cAns = findAns(question);
+        prevQuestion = preQAA.get(0).getText();
+        cAns = findAns(prevQuestion);
 
         if (cAns == null) {preQAA.get(1).click();}
-        else {
-            for (int i = 0; i < preQAA.size() - 1; i++) {
+        else {for (i = 0; i < preQAA.size() - 1; i++) {
                 pAns = preQAA.get(i + 1);
-                if (pAns.getText().equals(cAns)) {
-                    pAns.click();
-                }
-            }
-
+                if (pAns.getText().equals(cAns)) {pAns.click();}}
             driver.findElement(By.xpath("//div[@class='sc-kOnlKp dIDLaR']")).click();
         }
     }
 
-    public void learn() {}
+    public void learn() {
+        List<WebElement> preCorrectAns = driver.findElements(By.xpath("//span[@class='notranslate lang-en']"));
+        String correctAns;
+
+        if (!preCorrectAns.isEmpty()){
+            correctAns = preCorrectAns.get(0).getText();
+            words.put(prevQuestion, correctAns);
+        }
+        pressEnter();
+    }
+
+    public void pressEnter() {driver.findElement(By.xpath("//html")).sendKeys(Keys.ENTER);}
 }
--- src/gimkitBot/Classic.java
+++ src/gimkitBot/Classic.java
@@ -2,39 +2,27 @@ package gimkitBot;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
+import org.openqa.selenium.InvalidSelectorException;
 
 public class Classic extends BasicFunctions{
     private ChromeDriver driver;
 
-    public Classic(String id, String name) {
-        super(id, name);
-        driver = getDriver();
-    }
+    public Classic(String id, String name) {super(id, name);driver = getDriver();}
 
     public void start() throws InterruptedException {
         super.log_in();
-
         if (!driver.findElements(By.xpath("//span[@class='anticon anticon-close-circle']")).isEmpty()) {return;}
-
         startBot();
     }
 
     private void startBot() throws InterruptedException {
-        while (true) {
+        while (true) {try {
             if (!driver.findElements(By.xpath("//div[@style='height: 100%; width: 100%; display: flex; justify-content: center; align-items: center; padding: 7px 6%; box-sizing: border-box; font-weight: 900; font-size: 25px;']")).isEmpty()) {
                 multipleChoice();
-                Thread.sleep(1000);
-            }
+                Thread.sleep(1000);}
             else if (!driver.findElements(By.xpath("//div[@style='height: 100%; width: 100%; display: flex; justify-content: center; align-items: center; padding: 15px 6%; box-sizing: border-box; font-weight: 900; font-size: 40px;']")).isEmpty()) {
                 learn();
-            }
-            else if (!driver.findElements(By.xpath("//style=[@display: block; white-space: nowrap;']")).isEmpty()) {
-                driver.findElement(By.xpath("//")).sendKeys(Keys.ENTER);
-            }
-        }
-    }
-    public void stop() {
-        driver.quit();
-    }
+            }} catch(InvalidSelectorException e) {}}}
+    public void stop() {driver.quit();}
 }
